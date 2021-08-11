786cdd4d21bf3c9aec47a802f781e01238e8793a
PedroStival <pedro@vocebemonline.com.br>
2021-08-10 21:54:18 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style lang="scss">
+@import "~bootstrap-icons/font/bootstrap-icons.css";
+@import "~apexcharts/dist/apexcharts.css";
+@import "~quill/dist/quill.snow.css";
+@import "~animate.css";
+@import "~sweetalert2/dist/sweetalert2.css";
+@import "~nouislider/distribute/nouislider.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+@import "~socicon/css/socicon.css";
+@import "~line-awesome/dist/line-awesome/css/line-awesome.css";
+@import "~@yaireo/tagify/src/tagify.scss";
+@import "~dropzone/dist/dropzone.css";
+@import "~@vueform/multiselect/themes/default.css";
+@import "~prism-themes/themes/prism-shades-of-purple.css";
+@import "~element-plus/lib/theme-chalk/index.css";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// Main demo style scss
+@import "assets/sass/plugins";
+@import "assets/sass/style";
 </style>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import { useStore } from "vuex";
+import { Mutations } from "@/store/enums/StoreEnums";
+
+export default defineComponent({
+  name: "app",
+  setup() {
+    const store = useStore();
+
+    onMounted(() => {
+      /**
+       * this is to override the layout config using saved data from localStorage
+       * remove this to use config only from static config (@/core/config/DefaultLayoutConfig.ts)
+       */
+      store.commit(Mutations.OVERRIDE_LAYOUT_CONFIG);
+    });
+  }
+});
+</script>
